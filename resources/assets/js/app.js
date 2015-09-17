f6798244e924a275a1bba6838605be7eb0a77fdc
Chris Loftus <chrisloft@gmail.com>
2015-09-17 20:57:00 +0100

    handle user token expiry

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,7 +5,50 @@
         'satellizer',
         'angucomplete-alt'
     ])
-    .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    .config(function($stateProvider, $urlRouterProvider, $authProvider, $httpProvider, $provide) {
+
+        function redirectWhenLoggedOut($q, $injector) {
+
+            return {
+
+                responseError: function(rejection) {
+
+                    // Need to use $injector.get to bring in $state or else we get
+                    // a circular dependency error
+                    var $state = $injector.get('$state');
+
+                    // Instead of checking for a status code of 400 which might be used
+                    // for other reasons in Laravel, we check for the specific rejection
+                    // reasons to tell us if we need to redirect to the login state
+                    var rejectionReasons = ['token_not_provided', 'token_expired',
+                        'token_absent', 'token_invalid'];
+
+                    // Loop through each rejection reason and redirect to the login
+                    // state if one is encountered
+                    angular.forEach(rejectionReasons, function(value, key) {
+
+                        if(rejection.data.error === value) {
+
+                            // If we get a rejection corresponding to one of the reasons
+                            // in our array, we know we need to authenticate the user so
+                            // we can remove the current user from local storage
+                            localStorage.removeItem('user');
+
+                            // Send the user to the auth state so they can login
+                            $state.go('auth');
+                        }
+                    });
+
+                    return $q.reject(rejection);
+                }
+            };
+        }
+
+        // Setup for the $httpInterceptor
+        $provide.factory('redirectWhenLoggedOut', redirectWhenLoggedOut);
+
+        // Push the new factory onto the $http interceptor array
+        $httpProvider.interceptors.push('redirectWhenLoggedOut');
 
         // Satellizer configuration that specifies which API
         // route the JWT should be retrieved from
--- resources/views/partials/auth.php
+++ resources/views/partials/auth.php
@@ -4,10 +4,10 @@
             <h3>Login</h3>
             <form ng-submit="auth.login()">
                 <div class="form-group">
-                    <input type="email" class="form-control" placeholder="Email" ng-model="auth.email">
+                    <input type="email" name="email" class="form-control" placeholder="Email" ng-model="auth.email">
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password" ng-model="auth.password">
+                    <input type="password" name="password" class="form-control" placeholder="Password" ng-model="auth.password">
                 </div>
                 <button class="btn btn-primary">Submit</button>
             </form>
